d7887c8b86114e3f6fe72ec33fe35136e4df9b7b
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-10-09 14:01:20 +1100

    setup DataGrid for Team page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/topbar";
 import Sidebar from "./scenes/global/sidebar";
-import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/team";
+import Dashboard from "./scenes/dashboard/";
+import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
@@ -28,7 +28,7 @@ function App() {
 						<Topbar />
 						<Routes>
 							<Route path="/" element={<Dashboard />} />
-							{/* <Route path="/team" element={<Team />} /> */}
+							<Route path="/team" element={<Team />} />
 							{/* <Route path="/contacts" element={<Contacts />} /> */}
 							{/* <Route path="/invoices" element={<Invoices />} /> */}
 							{/* <Route path="/form" element={<Form />} /> */}
